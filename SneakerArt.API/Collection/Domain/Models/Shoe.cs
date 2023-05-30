13f0d33f890b9a8b80ae3e71520488d193ff1f69
FrancoSurco <u202015132@upc.edu.pe>
2023-05-30 13:43:54 -0500

    feat: Implement base API

---
+++ SneakerArt.API/Collection/Domain/Models/Shoe.cs
@@ -0,0 +1,14 @@
+﻿namespace SneakerArt.API.Collection.Domain.Models;
+
+public class Shoe
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string Img { get; set; }
+
+    
+    //Relationships
+    
+}
