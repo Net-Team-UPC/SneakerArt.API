ce0fd94489145b7bcb8d3319744d5f74c50b77f2
FrancoSurco <u202015132@upc.edu.pe>
2023-05-30 16:19:15 -0500

    feat: Added Shoe Repository

---
+++ SneakerArt.API/Collection/Domain/Repositories/IShoeRepository.cs
@@ -0,0 +1,12 @@
+﻿using SneakerArt.API.Collection.Domain.Models;
+
+namespace SneakerArt.API.Collection.Domain.Repositories;
+
+public interface IShoeRepository
+{
+    Task<IEnumerable<Shoe>> ListAsync();
+    Task AddAsync(Shoe shoe);
+    Task<Shoe> FindByIdAsync(int id);
+    void Update(Shoe shoe);
+    void Remove(Shoe shoe);
+}
