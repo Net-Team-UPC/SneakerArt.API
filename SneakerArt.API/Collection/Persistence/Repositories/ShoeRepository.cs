11461be31992708bb8e6741529080e5ac6efb6cb
FrancoSurco <u202015132@upc.edu.pe>
2023-05-30 18:53:50 -0500

    feat: Added ShoeRespository

---
+++ SneakerArt.API/Collection/Persistence/Repositories/ShoeRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SneakerArt.API.Collection.Domain.Models;
+using SneakerArt.API.Collection.Domain.Repositories;
+using SneakerArt.API.Shared.Persistence.Contexts;
+using SneakerArt.API.Shared.Persistence.Repositories;
+
+namespace SneakerArt.API.Collection.Persistence.Repositories;
+
+public class ShoeRepository : BaseRepository, IShoeRepository
+{
+    public ShoeRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Shoe>> ListAsync()
+    {
+        return await _context.Shoes.ToListAsync();
+    }
+
+    public async Task AddAsync(Shoe shoe)
+    {
+        await _context.Shoes.AddAsync(shoe);
+    }
+
+    public async Task<Shoe> FindByIdAsync(int id)
+    {
+        return await _context.Shoes.FindAsync(id);
+    }
+
+    public void Update(Shoe shoe)
+    {
+        _context.Shoes.Update(shoe);
+    }
+
+    public void Remove(Shoe shoe)
+    {
+        _context.Shoes.Remove(shoe);
+    }
+}
