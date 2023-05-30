9368912b160b69432e0da722c92720d1e72ee386
FrancoSurco <u202015132@upc.edu.pe>
2023-05-30 16:37:03 -0500

    feat: added Shoe and SaveShoe Resource

---
+++ SneakerArt.API/Collection/Resources/SaveShoeResource.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SneakerArt.API.Collection.Resources;
+
+public class SaveShoeResource
+{
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public string Description { get; set; }
+    
+    [Required]
+    public string Price { get; set; }
+    
+    [Required]
+    public string Img { get; set; }
+}
