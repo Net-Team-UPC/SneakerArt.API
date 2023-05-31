4483c8a4bbe4c0a41f117e8e6a96d0f12b4feab5
FrancoSurco <u202015132@upc.edu.pe>
2023-05-30 21:03:19 -0500

    feat: Added ShoesController

--- SneakerArt.API/Collection/Interfaces/Rest/Controllers/ShoesController.cs
+++ SneakerArt.API/Collection/Interfaces/Rest/Controllers/ShoesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using SneakerArt.API.Collection.Domain.Models;
 using SneakerArt.API.Collection.Domain.Services;
 using SneakerArt.API.Collection.Resources;
+using SneakerArt.API.Shared.Extensions;
 
 namespace SneakerArt.API.Collection.Interfaces.Rest.Controllers;
 
@@ -28,5 +29,56 @@ public class ShoesController : ControllerBase
         var shoes = await _shoeService.ListAsync();
         var resources = _mapper.Map<IEnumerable<Shoe>, IEnumerable<ShoeResource>>(shoes);
         return resources;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveShoeResource resource)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState.GetErrorMessages());
+        }
+
+        var shoe = _mapper.Map<SaveShoeResource, Shoe>(resource);
+        var result = await _shoeService.SaveAsync(shoe);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var shoeResource = _mapper.Map<Shoe, ShoeResource>(result.Resource);
+        
+        return Created(nameof(PostAsync), shoeResource);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveShoeResource resource)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState.GetErrorMessages());
+        }
+        
+        var shoe = _mapper.Map<SaveShoeResource, Shoe>(resource);
+        var result = await _shoeService.UpdateAsync(id,shoe);
+        
+        if (!result.Success)
+            return BadRequest(result.Message);
+        
+        var shoeResource = _mapper.Map<Shoe, ShoeResource>(result.Resource);
+        
+        return Ok(shoeResource);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _shoeService.DeleteAsync(id);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var shoeResource = _mapper.Map<Shoe, ShoeResource>(result.Resource);
+
+        return Ok(shoeResource);
     }
 }
