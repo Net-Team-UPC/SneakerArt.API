f5eaac558bcad6ba1c08dbf9fccd5e886fb307ee
FrancoSurco <u202015132@upc.edu.pe>
2023-05-30 20:38:44 -0500

    feat: Added ShoesController

---
+++ SneakerArt.API/Collection/Interfaces/Rest/Controllers/ShoesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Mime;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SneakerArt.API.Collection.Domain.Models;
+using SneakerArt.API.Collection.Domain.Services;
+using SneakerArt.API.Collection.Resources;
+
+namespace SneakerArt.API.Collection.Interfaces.Rest.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class ShoesController : ControllerBase
+{
+    private readonly IShoeService _shoeService;
+    private readonly IMapper _mapper;
+
+    public ShoesController(IShoeService shoeService, IMapper mapper)
+    {
+        _shoeService = shoeService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<ShoeResource>), 200)]
+    public async Task<IEnumerable<ShoeResource>> GetAllAsync()
+    {
+        var shoes = await _shoeService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Shoe>, IEnumerable<ShoeResource>>(shoes);
+        return resources;
+    }
+}
