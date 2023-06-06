96236d506a43851f4f321f70139f2ccbc2b3b694
FrancoSurco <u202015132@upc.edu.pe>
2023-06-05 22:31:32 -0500

    test: Added step definition for shoe service feature test

--- SneakerArt.API.Tests/Steps/ShoesServiceStepDefinitions.cs
+++ SneakerArt.API.Tests/Steps/ShoesServiceStepDefinitions.cs
@@ -1,29 +1,61 @@
-﻿namespace SneakerArt.API.Tests.Steps;
+﻿using System.Net;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using SneakerArt.API.Collection.Resources;
+using SpecFlow.Internal.Json;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace SneakerArt.API.Tests.Steps;
 
 [Binding]
-public class ShoesServiceStepDefinitions
+public class ShoesServiceStepDefinitions : WebApplicationFactory<Program>
 {
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public ShoesServiceStepDefinitions(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+    
+    private HttpClient Client { get; set; }
+    private Uri BaseUri { get; set; }
+    
+    private Task<HttpResponseMessage> Response { get; set; }
+
     [Given(@"the Endpoint https://localhost:(.*)/api/v(.*)/shoes is available")]
     public void GivenTheEndpointHttpsLocalhostApiVShoesIsAvailable(int port, int version)
     {
-        ScenarioContext.StepIsPending();
+        BaseUri = new Uri($"https://localhost:{port}/api/v{version}/shoes");
+        Client = _factory.CreateClient(new WebApplicationFactoryClientOptions { BaseAddress = BaseUri });
+        
     }
 
     [When(@"a Post Request is sent")]
-    public void WhenAPostRequestIsSent(Table table)
+    public void WhenAPostRequestIsSent(Table saveShoeResource)
     {
-        ScenarioContext.StepIsPending();
+        var resource = saveShoeResource.CreateSet<SaveShoeResource>().First();
+        var content = new StringContent(resource.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
+        Response = Client.PostAsync(BaseUri, content);
     }
 
     [Then(@"A Response is Received with Status (.*)")]
-    public void ThenAResponseIsReceivedWithStatus(int p0)
+    public void ThenAResponseIsReceivedWithStatus(int expectedStatus)
     {
-        ScenarioContext.StepIsPending();
+        var expectedStatusCode = ((HttpStatusCode)expectedStatus).ToString();
+        var actualStatusCode = Response.Result.StatusCode.ToString();
+        Assert.Equal(expectedStatusCode, actualStatusCode);
     }
 
     [Then(@"a Shoe Resource is included in Response Body")]
-    public void ThenAShoeResourceIsIncludedInResponseBody(Table table)
+    public async void ThenAShoeResourceIsIncludedInResponseBody(Table expectedShoeResource)
     {
-        ScenarioContext.StepIsPending();
+        var expectedResource = expectedShoeResource.CreateSet<ShoeResource>().First();
+        var responseData = await Response.Result.Content.ReadAsStringAsync();
+        var resource = JsonConvert.DeserializeObject<ShoeResource>(responseData);
+        Assert.Equal(expectedResource.Name, resource.Name);
     }
 }
