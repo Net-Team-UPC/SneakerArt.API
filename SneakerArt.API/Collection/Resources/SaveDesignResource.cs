b6ea31936dfb7e384746a9ea6245a08e50f8072b
FrancoSurco <u202015132@upc.edu.pe>
2023-06-08 20:17:45 -0500

    feat: Added Save Design Resource

---
+++ SneakerArt.API/Collection/Resources/SaveDesignResource.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SneakerArt.API.Collection.Resources;
+
+public class SaveDesignResource
+{
+    [Required]
+    [MaxLength(50)]
+    public string Brand { get; set; }
+    
+    [Required]
+    public string Model { get; set; }
+    
+    [Required]
+    public string Color { get; set; }
+    
+    
+    public string Size { get; set; }
+    
+    [Required]
+    [MaxLength(200)]
+    public string Img { get; set; }
+}
