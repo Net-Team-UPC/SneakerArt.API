c1c2ce9f5223168f48604e303488075623cc8362
FrancoSurco <u202015132@upc.edu.pe>
2023-06-09 00:58:18 -0500

    feat: Added CommentResponse

---
+++ SneakerArt.API/Collection/Domain/Services/Communication/CommentResponse.cs
@@ -0,0 +1,15 @@
+﻿using SneakerArt.API.Collection.Domain.Models;
+using SneakerArt.API.Shared.Domain.Services.Communication;
+
+namespace SneakerArt.API.Collection.Domain.Services.Communication;
+
+public class CommentResponse : BaseResponse<Comment>
+{
+    public CommentResponse(string message) : base(message)
+    {
+    }
+
+    public CommentResponse(Comment resource) : base(resource)
+    {
+    }
+}
