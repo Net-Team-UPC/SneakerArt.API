37a5d481121bcccea022657c6c4d980235b9ace7
FrancoSurco <u202015132@upc.edu.pe>
2023-06-09 00:59:32 -0500

    feat: Added SaveCommentResource

---
+++ SneakerArt.API/Collection/Resources/SaveCommentResource.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SneakerArt.API.Collection.Resources;
+
+public class SaveCommentResource
+{
+    [Required]
+    [MaxLength(150)]
+    public string Name { get; set; }
+    
+    
+}
