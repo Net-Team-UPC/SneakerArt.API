06d438aa38a996448fc1d968e5bf157f76f12420
FrancoSurco <u202015132@upc.edu.pe>
2023-06-08 20:17:32 -0500

    feat: Added Design Resource

---
+++ SneakerArt.API/Collection/Resources/DesignResource.cs
@@ -0,0 +1,12 @@
+﻿namespace SneakerArt.API.Collection.Resources;
+
+public class DesignResource
+{
+    public int Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public string Size { get; set; }
+    public string Img { get; set; }
+
+}
