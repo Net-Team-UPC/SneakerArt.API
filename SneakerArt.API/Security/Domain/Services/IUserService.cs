cee44d93a44c5fb5ac16ab4ce4ffe71f2cad0681
PieroMarquez <u201816402@upc.edu.pe>
2023-06-22 16:51:18 -0500

    chore: Update NuGets

--- SneakerArt.API/Security/Domain/Services/IUserService.cs
+++ SneakerArt.API/Security/Domain/Services/IUserService.cs
@@ -6,7 +6,7 @@ namespace SneakerArt.API.Security.Domain.Services;
 public interface IUserService
 {
     Task<AuthenticateResponse> AuthenticateAsync(AuthenticateRequest model);
-    Task<IEnumerable<User>> ListAsync();
+    Task<IEnumerable<Models.User>> ListAsync();
     Task<User> GetByIdAsync(int id);
     Task RegisterAsync(RegisterRequest model);
     Task UpdateAsync(int id, UpdateRequest model);
